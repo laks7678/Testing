370577142ec7997e26cb2f2175e35a006c828539
laks7678 <52536053+laks7678@users.noreply.github.com>
2020-11-27 17:09:38 +0530

    Updating files

--- OUTPUT/Pyspark_code/anz_Synapse_ESI_PRV_LOAD_CHK_Pyspark.py
+++ OUTPUT/Pyspark_code/anz_Synapse_ESI_PRV_LOAD_CHK_Pyspark.py
@@ -42,9 +42,3 @@ server = cp.get('SQLSERVERDBConnection', 'server')
 connection = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={user};PWD={password}')
 
 
-query4 = "SELECT LOAD_LOG_KEY FROM $TGT_LOAD_LOGWHERE WORK_FLOW_NM='$WORK_FLOW_NM'AND PBLSH_IND = 'N'AND LOAD_END_DTM = ( SELECT MAX (LOAD_END_DTM) FROM $TGT_LOAD_LOG WHERE SUBJ_AREA_NM = TRIM('$SUBJ_AREA_NM') )"
-cursor = connection.cursor()
-cursor.execute(query4)
-for row in cursor:
-    print(row)
-
