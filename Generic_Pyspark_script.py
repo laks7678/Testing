84719cf0bf92f20bdb78c02cde2e564a65a06a97
laks7678 <52536053+laks7678@users.noreply.github.com>
2020-11-28 15:47:50 +0530

    Update Generic_Pyspark_script.py

--- Generic_Pyspark_script.py
+++ Generic_Pyspark_script.py
@@ -11,9 +11,11 @@ findspark.find()
 #Importing spark packages
 import pyspark
 from pyspark import SparkContext, SparkConf, SQLContext
+#Importing SparkSession
 from pyspark.sql import SparkSession
 import pyodbc
 import pandas as pd
+#Importing spark sql packages
 from pyspark.sql import functions as F
 from pyspark.sql.functions import lit, col
 from _io import StringIO
