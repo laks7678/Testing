f07da17f2bd5bc8f45d07bd1785b699582285f0e
laks7678 <52536053+laks7678@users.noreply.github.com>
2020-11-30 11:44:54 +0530

    Updating files

--- OUTPUT/Pyspark_code/anz_Synapse_ESI_PRV_LOAD_CHK_Pyspark.py
+++ OUTPUT/Pyspark_code/anz_Synapse_ESI_PRV_LOAD_CHK_Pyspark.py
@@ -1,25 +1,26 @@
 import sys
 import os
 import logging
+#Importing Config parser object to read property files
 import configparser
+#Connecting github using pygithub package
 from github import Github
 import findspark
 findspark.init()
 findspark.find()
+#Importing spark packages
 import pyspark
 from pyspark import SparkContext, SparkConf, SQLContext
+#Importing SparkSession
 from pyspark.sql import SparkSession
 import pyodbc
 import pandas as pd
+#Importing spark sql packages
 from pyspark.sql import functions as F
 from pyspark.sql.functions import lit, col
 from _io import StringIO
-
-spark=SparkSession.builder.appName("Building Pyspark code with Synapse statements embedded").getOrCreate()
-sc=spark.sparkContext
-
-
 cp = configparser.ConfigParser()
+#Get the github token from environment variables to access github repository
 g = Github(os.environ.get('GITHUB_TOKEN'))
 repo = g.get_user().get_repo( 'Testing' )
 files_and_dirs = [fd for fd in repo.get_dir_contents('/')]
@@ -31,9 +32,12 @@ while len(contents)>0:
         contents.extend(repo.get_contents(file_content.path))
     else :
         if file_content.name=='properties_1.ini':
-            cp.readfp(StringIO(file_content.decoded_content.decode()))
+            #Reading property file
+            cp.read_file(StringIO(file_content.decoded_content.decode()))
 
-
+#Creating spark session
+spark=SparkSession.builder.appName(cp.get('PySparkProp', 'appName')).getOrCreate()
+sc=spark.sparkContext
 database =cp.get('SQLSERVERDBConnection', 'database')
 user = cp.get('SQLSERVERDBConnection', 'user')
 password = cp.get('SQLSERVERDBConnection', 'password')
@@ -42,9 +46,10 @@ server = cp.get('SQLSERVERDBConnection', 'server')
 connection = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={user};PWD={password}')
 
 
-query4 = "SELECT LOAD_LOG_KEY FROM $TGT_LOAD_LOGWHERE WORK_FLOW_NM='$WORK_FLOW_NM'AND PBLSH_IND = 'N'AND LOAD_END_DTM = ( SELECT MAX (LOAD_END_DTM) FROM $TGT_LOAD_LOG WHERE SUBJ_AREA_NM = TRIM('$SUBJ_AREA_NM') )"
+#Creating dataframes for parsing the tables#Creating dataframes for parsing the tables#Creating dataframes for parsing the tablesdf2 = "SELECT LOAD_LOG_KEY FROM $TGT_LOAD_LOGWHERE WORK_FLOW_NM='$WORK_FLOW_NM'AND PBLSH_IND = 'N'AND LOAD_END_DTM = ( SELECT MAX (LOAD_END_DTM) FROM $TGT_LOAD_LOG WHERE SUBJ_AREA_NM = TRIM('$SUBJ_AREA_NM') )"
 cursor = connection.cursor()
 cursor.execute(query4)
 for row in cursor:
     print(row)
 
+#Creating dataframes for parsing the tables
