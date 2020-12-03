e98556fc64defc3156b9159a1b563397a2e91b3a
laks7678 <52536053+laks7678@users.noreply.github.com>
2020-12-03 12:52:34 +0530

    Updating files

--- OUTPUT/Pyspark_code/anz_Synapse_ESI_PRV_LOAD_CHK_Pyspark.py
+++ OUTPUT/Pyspark_code/anz_Synapse_ESI_PRV_LOAD_CHK_Pyspark.py
@@ -49,7 +49,7 @@ connection = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database
 #Creating dataframes for parsing the tables#Creating dataframes for parsing the tables#Creating dataframes for parsing the tables
 df2 = "SELECT LOAD_LOG_KEY FROM $TGT_LOAD_LOGWHERE WORK_FLOW_NM='$WORK_FLOW_NM'AND PBLSH_IND = 'N'AND LOAD_END_DTM = ( SELECT MAX (LOAD_END_DTM) FROM $TGT_LOAD_LOG WHERE SUBJ_AREA_NM = TRIM('$SUBJ_AREA_NM') )"
 cursor = connection.cursor()
-cursor.execute(query4)
+cursor.execute(df4)
 for row in cursor:
     print(row)
 
